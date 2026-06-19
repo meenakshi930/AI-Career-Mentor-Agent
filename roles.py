a33eddbd3e0351268fd5c72f2c0709dfff67b52a
meenakshi930 <meenakshi.gupta_cs.aiml24@gla.ac.in>
2026-06-19 23:53:02 +0530

    Initial version of AI Career Mentor Agent

---
+++ roles.py
@@ -0,0 +1,40 @@
+roles = {
+    "AI Engineer": {
+        "Python": 30,
+        "Statistics": 20,
+        "Machine Learning": 30,
+        "Deep Learning": 10,
+        "Projects": 10
+    },
+
+    "Data Scientist": {
+        "Python": 25,
+        "Statistics": 25,
+        "Machine Learning": 20,
+        "SQL": 15,
+        "Projects": 15
+    },
+
+    "Data Analyst": {
+        "SQL": 30,
+        "Excel": 25,
+        "Power BI": 25,
+        "Python": 20
+    },
+
+    "Software Engineer": {
+        "DSA": 30,
+        "OOP": 20,
+        "DBMS": 20,
+        "Operating Systems": 15,
+        "Projects": 15
+    },
+
+    "Web Developer": {
+        "HTML": 20,
+        "CSS": 20,
+        "JavaScript": 30,
+        "React": 15,
+        "Projects": 15
+    }
+}
